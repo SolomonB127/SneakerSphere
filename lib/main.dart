ff3f640c34dfe0eee5f120d437a29124ce3818da
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-25 17:58:37 +0100

    Added the routes to homepage - For now

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_sphere/Pages/home_page.dart';
 
 import 'Pages/welcome_page.dart';
 
@@ -11,9 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      home: const WelcomePage(),
+      routes: {
+        '/homepage': (context) => const HomePage(),
+      },
     );
   }
 }
