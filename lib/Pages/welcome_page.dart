6b8cec6924caaa9a1b65fbc68ae66ed5acd16750
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-25 17:57:02 +0100

    Finished coding Welcome/Intro Page

--- lib/Pages/welcome_page.dart
+++ lib/Pages/welcome_page.dart
@@ -5,6 +5,69 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: <Widget>[
+              // Logo image
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  '../assets/Images/logo.png',
+                  width: 200.0,
+                ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+
+              // Title
+              const Text(
+                "Just Do It",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+
+              // Sub-title
+              const Text(
+                "Brand new Snickers and custom kicks made with premium quality.",
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 48.0,
+              ),
+
+              // Shop now link btn
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/homepage');
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
