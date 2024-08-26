3a081db93502ee0a3f02a6f1b04162b58c8c0be2
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-26 19:33:14 +0100

    Transformed Homepage into a Stateful Widget and created variables and method which will help in the tracking of bottom nav bar pages

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:sneaker_sphere/Components/bottom_bar.dart';
 
-class HomePage extends StatelessWidget {
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Bottom navigation bar
+  int _selectedIndex = 0;
+
+  // Method to update selected index based on usr tap
+  navigationBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // Pages to display
+  final List<Widget> _bottomNavPages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      bottomNavigationBar: const BottomBar(),
+      bottomNavigationBar: BottomBar(
+        onTabChange: (index) => navigationBottomBar(index),
+      ),
     );
   }
 }
