2b2fd77f0a6a33a636e10a5a58e8ce6e883d2db6
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-26 18:58:25 +0100

    Imported the Google Nav(Gnav) widget

--- lib/Components/bottom_bar.dart
+++ lib/Components/bottom_bar.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomBar extends StatelessWidget {
   const BottomBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const GNav(
+      tabs: [
+        GButton(
+          icon: Icons.home,
+          text: "Shop",
+        ),
+        GButton(
+          icon: Icons.shopping_cart,
+          text: "Cart",
+        )
+      ],
+    );
   }
 }
