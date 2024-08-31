5dd50e9eeeb1b4a2f21e8e5fea6810914d53750f
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-31 13:01:51 +0100

    Added a search bar to page

--- lib/Pages/shop_page.dart
+++ lib/Pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,6 +11,30 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Shop"));
+    return Column(
+      children: <Widget>[
+        // Search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: const TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+                icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                )),
+          ),
+        )
+
+        // Message text
+
+        // Hot Picks/Trending
+      ],
+    );
   }
 }
