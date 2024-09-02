ec3ee9d672248fb71e3f6509b9501a6f75e40e80
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-02 13:27:33 +0100

    Changed description of shoes

--- lib/modules/cart_data.dart
+++ lib/modules/cart_data.dart
@@ -5,26 +5,25 @@ class Cart extends ChangeNotifier {
   // list of shoes for sale
   List<Shoe> shoeShop = [
     Shoe(
-        name: "Air Force",
+        name: "Superstar",
         price: "236",
         imgPath: "assets/Images/img3.png",
-        description: "The forward-thinking design of his latest shoe"),
+        description: "Quick cuts, explosive power."),
     Shoe(
         name: "Louis Vuitton",
         price: "500",
         imgPath: "assets/Images/img2.png",
-        description:
-            "You've got the hops and the speed-lace up in shoes that enhances what you bring to the court"),
+        description: "Fast, responsive, unbeatable."),
     Shoe(
         name: "Chuck Taylor",
         price: "240",
         imgPath: "assets/Images/img1.png",
-        description: "A secure mid foot strap for"),
+        description: "Light, precise, unstoppable."),
     Shoe(
         name: "Air Force",
         price: "236",
         imgPath: "assets/Images/img4.png",
-        description: "The forward-thinking design of his latest shoe"),
+        description: "Relentless pace, no limits."),
   ];
 
   // list of items in cart
