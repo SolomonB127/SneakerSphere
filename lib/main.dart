789339f4cfc02ed9e7b45546858632223a15df6d
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-02 13:02:27 +0100

    Initialize ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sneaker_sphere/Pages/home_page.dart';
 
 import 'Pages/welcome_page.dart';
+import 'modules/cart_data.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,12 +14,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const WelcomePage(),
-      routes: {
-        '/homepage': (context) => const HomePage(),
-      },
-    );
+    return ChangeNotifierProvider(
+        create: (context) => Cart(),
+        builder: (context, child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: const WelcomePage(),
+              routes: {
+                '/homepage': (context) => const HomePage(),
+              },
+            ));
   }
 }
