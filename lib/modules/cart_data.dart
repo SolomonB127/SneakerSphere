bc36d5b89c7e531b19297e6bf47ea23e18afc34c
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-02 13:03:24 +0100

    Extend ChangeNotifier into file

--- lib/modules/cart_data.dart
+++ lib/modules/cart_data.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:sneaker_sphere/modules/shoe_data.dart';
 
-class Cart {
+class Cart extends ChangeNotifier {
   // list of shoes for sale
   List<Shoe> shoeShop = [
     Shoe(
@@ -42,10 +43,12 @@ class Cart {
   // add items to cart
   void addItemToCart(Shoe shoe) {
     userCart.add(shoe);
+    notifyListeners();
   }
 
   // remove items to cart
   void removeItemFromCart(Shoe shoe) {
     userCart.remove(shoe);
+    notifyListeners();
   }
 }
