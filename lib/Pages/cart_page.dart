53f10af5049be91ec4277292ab217e60172a22b2
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-03 16:25:31 +0100

    Integrate getUserCart method to enable adding shoes to cart functionality.

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneaker_sphere/modules/shoe_data.dart';
+
+import '../Components/cart_item.dart';
+import '../modules/cart_data.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,8 +15,35 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Cart"),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          // Heading
+          children: <Widget>[
+            const Text(
+              "My Cart",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: value.getUserCart().length,
+              itemBuilder: (context, index) {
+                // get shoes
+                Shoe pickedShoe = value.getUserCart()[index];
+
+                // return cart item
+                return CartItem(
+                  shoe: pickedShoe,
+                );
+              },
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
