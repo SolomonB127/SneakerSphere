eb9ff75ffef5694b5767e2a436489c4278ec4c28
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-03 16:43:57 +0100

    Created a method that helps in removing items from the cart page

--- lib/Components/cart_item.dart
+++ lib/Components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneaker_sphere/modules/cart_data.dart';
 import 'package:sneaker_sphere/modules/shoe_data.dart';
 
 class CartItem extends StatefulWidget {
@@ -10,12 +12,24 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
+  // Remove item from cart
+  void removeFromCart() {
+    Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(widget.shoe.imgPath),
-      title: Text(widget.shoe.name),
-      subtitle: Text(widget.shoe.price),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[100], borderRadius: BorderRadius.circular(8)),
+      margin: const EdgeInsets.only(bottom: 10.0),
+      child: ListTile(
+        leading: Image.asset(widget.shoe.imgPath),
+        title: Text(widget.shoe.name),
+        subtitle: Text(widget.shoe.price),
+        trailing: IconButton(
+            onPressed: removeFromCart, icon: const Icon(Icons.delete)),
+      ),
     );
   }
 }
