1399e1146eed9ab1f420894528d3b1fc547b512c
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-03 16:45:41 +0100

    Added extra styles

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -19,6 +19,7 @@ class _CartPageState extends State<CartPage> {
       builder: (context, value, child) => Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           // Heading
           children: <Widget>[
             const Text(
